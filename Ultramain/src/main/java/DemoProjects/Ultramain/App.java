296ea409c22ad37d6b1ea5cfe2ed7594808d5cff
Thilakg1704 <thilakg399@gmail.com>
2024-08-26 15:16:45 +0530

    InitialCommit

---
+++ Ultramain/src/main/java/DemoProjects/Ultramain/App.java
@@ -0,0 +1,13 @@
+package DemoProjects.Ultramain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
